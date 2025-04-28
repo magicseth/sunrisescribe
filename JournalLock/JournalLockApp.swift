54b277e34eae9f3c4a2c6ab48804a3a175b74149
Seth Raphael <git@magicseth.com>
2025-04-27 20:56:24 -0700

    Initial Commit

---
+++ JournalLock/JournalLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalLockApp.swift
+//  JournalLock
+//
+//  Created by Seth Raphael on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct JournalLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
